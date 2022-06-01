2c78c6598a909194ab883a5d715bfba6d4c132ba
mkumar@solifetec.com <nishantkumar@solifetec.com>
2022-06-01 16:42:58 +0530

    medician change

--- src/app/component/resource/resource.component.ts
+++ src/app/component/resource/resource.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit,AfterContentInit } from '@angular/core';
 
 @Component({
   selector: 'app-resource',
   templateUrl: './resource.component.html',
   styleUrls: ['./resource.component.css']
 })
-export class ResourceComponent implements OnInit {
+export class ResourceComponent implements OnInit,OnChanges, AfterContentInit{
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log("intine")
   }
-
+  ngOnChanges(){
+console.log("changes")
+  }
+  ngAfterContentInit() {
+    console.log("hfsdfhgs")
+  }
 }
